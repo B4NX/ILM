48f708f6def5ec204cd933dd686fd9a8be73b46c
Nikko Rush <nikko.rush@gmail.com>
2015-05-08 16:45:15 -0700

    Initial

---
+++ GloveDriver/Arduino.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace GloveDriver
+{
+    class Arduino
+    {
+        SerialPort arduino;
+        public Arduino()
+        {
+            this.arduino = new SerialPort("COM4", 9600);
+            arduino.Open();
+        }
+
+        public byte[] ReadSignal()
+        {
+            int bytesToRead = this.arduino.BytesToRead;
+            byte[] data = new byte[bytesToRead];
+            arduino.Read(data, 0, bytesToRead);
+            return data;
+        }
+
+        public void SendSignal(string data)
+        {
+            arduino.Write(data);
+        }
+        public void Explode()
+        {
+
+        }
+    }
+}
---
+++ GloveDriver/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace GloveDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arduino board = new Arduino();
+            String ting = "aaaa";
+            board.SendSignal("aaaa");
+
+            foreach (byte b in board.ReadSignal())
+            {
+                Console.WriteLine(b);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
